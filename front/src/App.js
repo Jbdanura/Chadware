8df26d00b1a3c84351bf17491e33f2297090c080
unknown <danurabe@gmail.com>
2022-07-26 18:11:18 -0300

    front deal

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import logo from "./img.png"
+import Deal1 from "./deals/1.png"
+import Deal2 from "./deals/2.png"
+import Deal3 from "./deals/3.png"
+import Deal4 from "./deals/4.png"
+import Deal5 from "./deals/5.png"
 
 const App = () =>{
 
@@ -7,7 +12,6 @@ const App = () =>{
 
    return(
       <div>
-
          <div className="mobile-menu">
             <div className="hamburger" onClick={()=>setNavMobile(!navMobile)}>
                   <div className='line'></div>
@@ -43,6 +47,7 @@ const App = () =>{
                               <li>Monitor</li>
                               <li>Peripheric</li>
                               <li>Power supply</li>
+                              <li>Case</li>
                         </ul>
                   </div>
                   <div className="pages">
@@ -54,7 +59,46 @@ const App = () =>{
                   </div>
             </div>
             <div className="content">
-                  content
+                  <h3 className="deals-title">Best deals</h3>
+                  <div className="deals-products">
+                        <div className="product-grid-container">
+                              <div className="product-grid">
+                                    <div className="info">
+                                          <img className="info-img" src={Deal1}></img>
+                                          <p className="info-name">Gigabyte GeForce GTX 1660 SUPER OC 6GB GDDR6</p>
+                                          <p className="info-price">$300</p>
+                                    </div>
+                              </div>
+                              <div className="product-grid">
+                                    <div className="info">
+                                          <img className="info-img" src={Deal2}></img>
+                                          <p className="info-name">MSI PRO MP241X 23.8" LED FullHD 75Hz</p>
+                                          <p className="info-price">$120</p>
+                                    </div>
+                              </div>
+                              <div className="product-grid">
+                                    <div className="info">
+                                          <img className="info-img" src={Deal3}></img>
+                                          <p className="info-name">Tempest Spectra RGB Torre ATX Negra</p>
+                                          <p className="info-price">$35</p>
+                                    </div>
+                              </div>
+                              <div className="product-grid">
+                                    <div className="info">
+                                          <img className="info-img" src={Deal4}></img>
+                                          <p className="info-name">Intel Core i5-11400F 2.6 GHz</p>
+                                          <p className="info-price">$150</p>
+                                    </div>
+                              </div>
+                              <div className="product-grid">
+                                    <div className="info">
+                                          <img className="info-img" src={Deal5}></img>
+                                          <p className="info-name">Gigabyte B560M DS3H V2</p>
+                                          <p className="info-price">$82</p>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
             </div>
          </div>
          </div>
